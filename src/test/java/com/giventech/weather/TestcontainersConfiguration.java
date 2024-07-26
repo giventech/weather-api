3892748f45f4274aa3c1383bab9e56befcb1376a
Fabrice GINAPE <giventech@gmail.com>
2024-07-26 13:48:25 +1000

    Initial commit

---
+++ src/test/java/com/giventech/weather/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.giventech.weather;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
